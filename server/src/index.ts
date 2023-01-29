740d7e430111337ebdad3bde9161626cdc1acb2c
Mazen Sharkawy <mazenmaher1997@gmail.com>
2023-01-29 20:25:28 +0200

    add image details route

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,16 +14,35 @@ app.use(
     origin: "http://localhost:3000",
     credentials: true,
   })
-);
+  );
+  
+const IMGUR_BASE_URL = "https://api.imgur.com/3/gallery/"
 
-app.get("/", (req: Request, res: Response) => {
-  fetch("https://api.imgur.com/3/gallery/hot/viral/day/1?album_previews=true?albumPreviews=false&showMature=false",{
+app.get("/image/:image_id", (req: Request, res: Response) => {
+  const {image_id} = req.params 
+  console.log({image_id})
+  fetch(`https://api.imgur.com/3/gallery/album/${image_id}`,{
       headers:{
         "Authorization": "Client-ID d07ca043349696e"
       }
-    }).then(response=>response.json()).then(response=>{
+    }).then(response=>{
+      console.log({response})
+      return response.json()
+    }
+      ).then(response=>
+      res.json(response.data)
+    ).catch(err=>res.status(500).send(err))
+});
+
+app.get("/", (req: Request, res: Response) => {
+  const {sort, window, section} = req.query
+  fetch(IMGUR_BASE_URL + `${section}/${sort}/${window}/1?album_previews=true?albumPreviews=false&showMature=false`,{
+      headers:{
+        "Authorization": "Client-ID d07ca043349696e"
+      }
+    }).then(response=>response.json()).then(response=>
       res.json(response.data.slice(0,20))
-    }).catch(err=>res.status(500).send(err))
+    ).catch(err=>res.status(500).send(err))
 });
 
 const PORT = process.env.PORT || 5000;
